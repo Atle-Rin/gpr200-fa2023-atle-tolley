4e994a78128dea65cf4c3cf0d1e21af29c260e33
Atle-Rivian <atle.tolley@mymail.champlain.edu>
2023-11-07 14:19:40 -0500

    Cylinder vertices setup in progress positions and normals set up indices initially set up, reconfiguring setup loops required after vertex normals format changed UV coords not set up.

--- core/a-rt/procGen.cpp
+++ core/a-rt/procGen.cpp
@@ -4,15 +4,83 @@ namespace artLib {
 	ew::MeshData createSphere(float radius, int numSegments) {
 		ew::MeshData sphere;
 		for (int i = 0; i < numSegments; i++) {
-			sphere.vertices.push_back(ew::Vec3(i, i, i); //placeholder. replace with actual vertex position calculations later
+			sphere.vertices.push_back(ew::Vec3(i, i, i)); //placeholder. replace with actual vertex position calculations later
 		}
 
 		return sphere;
 	}
 	ew::MeshData createCylinder(float height, float radius, int numSegments) {
 		ew::MeshData cylinder;
+
+		float top = height / 2;
+		float bottom = top * -1;
+		ew::Vertex topV;
+		topV.pos = ew::Vec3(0, top, 0);
+		topV.normal = ew::Vec3(0, 1, 0);
+		cylinder.vertices.push_back(topV);
+		float angleStep = (3.14159265358979323 * 2) / numSegments;
+		for (int i = 0; i <= numSegments; i++) {
+			float angle = i * angleStep;
+			ew::Vertex vertex;
+			vertex.pos.x = cos(angle) * radius;
+			vertex.pos.z = sin(angle) * radius;
+			vertex.pos.y = top;
+			vertex.normal = ew::Vec3(0, 1, 0);
+			cylinder.vertices.push_back(vertex);
+		}
+		for (int i = 0; i <= numSegments; i++) {
+			float angle = i * angleStep;
+			ew::Vertex vertex;
+			vertex.pos.x = cos(angle) * radius;
+			vertex.pos.z = sin(angle) * radius;
+			vertex.pos.y = top;
+			vertex.normal = vertex.pos - topV.pos;
+			cylinder.vertices.push_back(vertex);
+		}
+		ew::Vertex botV;
+		botV.pos = ew::Vec3(0, bottom, 0);
+		botV.normal = ew::Vec3(0, -1, 0);
+		cylinder.vertices.push_back(botV);
+		for (int i = 0; i <= numSegments; i++) {
+			float angle = i * angleStep;
+			ew::Vertex vertex;
+			vertex.pos.x = cos(angle) * radius;
+			vertex.pos.z = sin(angle) * radius;
+			vertex.pos.y = bottom;
+			vertex.normal = vertex.pos - botV.pos;
+			cylinder.vertices.push_back(vertex);
+		}
+		for (int i = 0; i <= numSegments; i++) {
+			float angle = i * angleStep;
+			ew::Vertex vertex;
+			vertex.pos.x = cos(angle) * radius;
+			vertex.pos.z = sin(angle) * radius;
+			vertex.pos.y = bottom;
+			vertex.normal = ew::Vec3(0, -1, 0);
+			cylinder.vertices.push_back(vertex);
+		}
+
+		unsigned int topC = 0;
+		unsigned int start = 1;
+		unsigned int columns = numSegments + 1;
+		columns *= 2;
 		for (int i = 0; i < numSegments; i++) {
-			cylinder.vertices.push_back(ew::Vec3(i, i, i); //placeholder. replace with actual vertex position calculations later
+			cylinder.indices.push_back(topC);
+			cylinder.indices.push_back(start + i);
+			cylinder.indices.push_back(start + i + 1);
+		}
+		for (int i = 0; i < numSegments; i++) {
+			cylinder.indices.push_back(botC);
+			cylinder.indices.push_back(start + i + botC);
+			cylinder.indices.push_back(start + i + botC + 1);
+		}
+		for (int i = 0; i < numSegments; i++) {
+			cylinder.indices.push_back(start + i);
+			cylinder.indices.push_back(start + i + 1);
+			cylinder.indices.push_back(start + i + botC);
+			cylinder.indices.push_back(start + i + 1);
+			cylinder.indices.push_back(start + i + botC);
+			cylinder.indices.push_back(start + i + botC + 1);
 		}
 
 		return cylinder;
